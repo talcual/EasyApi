ebb9007cbd094551a505ac4a96d2f46d037dad7b
Luis Toscano <commentariosweb@gmail.com>
2015-12-23 13:31:48 -0500

    Update _bootstrap.php
    Add support for ionic frameworks apps, with payload request.

--- config/_bootstrap.php
+++ config/_bootstrap.php
@@ -52,30 +52,55 @@ function factory($libs, $get = ''){
 
 // Runner Router
 function runner(){
- GLOBAL $onlypost, $qs;
+ GLOBAL $onlypost, $qs, $config;
 
-  if($_POST){
-    $vars = parseVars($_POST);
-    if (in_array($vars['action'], $onlypost)) {
-      run($vars['getting'],$vars['action']);
-    }else{
-      echo 'This is a D\'oh';
-    }
-  }
+    if($config['appserver'] == 'ionic'){
+      if (isset($_SERVER['HTTP_ORIGIN'])) {
+          header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+          header('Access-Control-Allow-Credentials: true');
+          header('Access-Control-Max-Age: 86400');    // cache for 1 day
+      }
 
-  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if($qs == ''){
-      run('','hello');
-    }else{
-      if($_GET){
-        $vars = parseVars($_GET);
-        if (!in_array($vars['action'], $onlypost)) {
+      if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+        }
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            exit(0);
+        }
+      }
+
+     $payload = file_get_contents("php://input");
+     $data = json_decode($payload);
+
+     run($data->getting,$data->action);
+
+  }else{
+      if($_POST){
+        $vars = parseVars($_POST);
+        if (in_array($vars['action'], $onlypost)) {
           run($vars['getting'],$vars['action']);
         }else{
           echo 'This is a D\'oh';
         }
       }
-    }
+
+      if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if($qs == ''){
+          run('','hello');
+        }else{
+          if($_GET){
+            $vars = parseVars($_GET);
+            if (!in_array($vars['action'], $onlypost)) {
+              run($vars['getting'],$vars['action']);
+            }else{
+              echo 'This is a D\'oh';
+            }
+          }
+        }
+      }
   }
 
 }
